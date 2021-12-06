10ca59dec8b4853bacced9b3164e4200e6e9f4f8
jisoo <93499095+amisu1203@users.noreply.github.com>
2021-12-06 10:25:31 +0900

    Update 12.7-다양한 함수의 형태.js

--- 12-함수/12.7-다양한 함수의 형태.js
+++ 12-함수/12.7-다양한 함수의 형태.js
@@ -154,7 +154,7 @@ function repeat2(n) {
 }
 repeat2(5); // 1 3
 
-// (12-50)
+// (12-51)
 // 외부에서 전달받은 f를 n만큼 반복호출하는 함수
 function repeat3(n, f) {
     for(var i = 0; i < n; i++) {
@@ -176,5 +176,79 @@ var logOods = function (i) {
 }
 
 repeat3(5, logOods); // 1 3
+
+// (12-52)
+// 익명함수 리터럴은 repeat함수를 호출할 때마다 평가되어 함수 객체를 생성한다.
+repeat3(5, function(i) {
+    if (i % 2) console.log(i);
+}); // 3
+
+// (12-53)
+// 콜백함수를 전달받는 함수가 자주 호출된다면 외부에서 콜백함수를 정의하고 고차함수에 참조를 전달하는게 효율적이다.
+var logOods = function(i) {
+    if (i % 2) {
+        console.log(i)
+    }
+};
+
+repeat3(3, logOods); // 1
+
+// ( 12-54)
+// 콜백함수를 이용한 이벤트 처리
+// myButton을 클릭하면 콜백 함수를 실행한다.
+// document.getElementById('myButton').addEventListener('click', function(){
+//     console.log('button clicked');
+// });
+
+// 콜백함수를 이용한 비동기 처리
+setTimeout(function () {
+    console.log('1초 경과');
+}, 1000);
+
+// (12-55)
+// 콜백함수를 사용하는 고차 함수 map
+var res = [1, 2, 3].map(function (item) {
+    return item * 2;
+});
+console.log(res); // [2, 4, 6]
+
+// 콜백함수를 사용하는 고차함수 filter
+res = [1, 2, 3].filter(function (item) {
+    return item % 2;
+});
+console.log(res); // [1, 3]
+
+// 콜백함수를 사용하는 고차함수 reduce
+res = [1, 2, 3].reduce(function (acc, cur) {
+    return acc + cur;
+}, 0);
+console.log(res); // 6
+
+// (12-56)
+var count = 0;
+// 순수 함수 increase는 동일한 인수가 전달되면 언제나 동일한 값을 반환한다.
+function increase (n) {
+    return n++;
+}
+// 순수함수가 반환한 값을 변수에 재할당해서 상태를 변경
+count = increase(count);
+console.log(count); // 1
+
+count = increase(count);
+console.log(count); // 2
+
+// (12-57) : 비순수 함수
+var count = 0;
+function increase() {
+    return ++count;
+    // 외부 상태를 변경하며 외부 상태에 의존한다.
+}
+increase(); 
+console.log(count); // 1
+
+increase();
+console.log(count); // 2
+
+
 
 
